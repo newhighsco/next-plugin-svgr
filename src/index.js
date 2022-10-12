f565cd09ab36dc4b2d4ef83650bcba5323f2f256
Ben Edwards <hello@benedfit.com>
2022-10-12 12:02:33 +0100

    fix: prevent invalid config warnings (#814)
    Closes #759

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const addSvgrLoaders = require('./webpack')
 
 module.exports = (nextConfig = {}) => {
-  return Object.assign({}, nextConfig, {
+  const { svgrOptions, ...rest } = nextConfig
+  return Object.assign({}, rest, {
     webpack: addSvgrLoaders(nextConfig)
   })
 }
